7a24222ef392281bc5bf90d0c477af8fedc24844
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-09-12 18:31:41 -0400

    Updated Gitignore

--- food_app/info/views.py
+++ food_app/info/views.py
@@ -2,10 +2,11 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def index(request: HttpRequest):
+def index(request: HttpRequest) -> HttpResponse:
     if request.user.is_authenticated:
         return HttpResponse(f"Hello, {request.user.get_username()}")
     else:
         return HttpResponse(
-            "This is the info screen. Here, we will tell users about our app and include a button which will redirect them to the login screen."
+            "This is the info screen. Here, we will tell users about our app "
+            "and include a button which will redirect them to the login screen."
         )

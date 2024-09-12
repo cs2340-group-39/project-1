18d5fe2020b47826b8a5efe5bfdbaef2cb855c2e
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-09-12 19:44:45 -0400

    Added app_name

--- food_app/info/urls.py
+++ food_app/info/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 
 from .views import index
 
+app_name = "info"
 urlpatterns = [
     path("", index),
 ]
--- food_app/maps/urls.py
+++ food_app/maps/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 
 app_name = "maps"
+
 urlpatterns = []

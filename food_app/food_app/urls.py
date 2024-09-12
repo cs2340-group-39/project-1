52c5a6a91c9d308f92788a5d706bd9bd29f72bf3
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-09-12 19:15:30 -0400

    Fixed merge conflict

--- food_app/food_app/urls.py
+++ food_app/food_app/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path("", include("info.urls")),
     path("maps/", include("maps.urls")),
 ]

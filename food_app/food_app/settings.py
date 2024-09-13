46c556143cc8a173ad9f4b6e174eacdad30b188c
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-09-13 00:22:39 -0400

    Added some email setup

--- food_app/food_app/settings.py
+++ food_app/food_app/settings.py
@@ -138,6 +138,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# Django Allauth Setup
 SITE_ID = 2
 
 SOCIALACCOUNT_PROVIDERS = {
@@ -154,3 +155,11 @@ SOCIALACCOUNT_PROVIDERS = {
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv("GOOGLE_USERNAME")
+EMAIL_HOST_PASSWORD = os.getenv("GOOGLE_PASSWORD")
--- food_app/info/views.py
+++ food_app/info/views.py
@@ -7,6 +7,7 @@ def index(request: HttpRequest) -> HttpResponse:
         return HttpResponse(f"Hello, {request.user.get_username()}")
     else:
         return HttpResponse(
-            "This is the info screen. Here, we will tell users about our app "
-            "and include a button which will redirect them to the login screen."
+            "This is the info screen. You are seeing this screen because you "
+            "are not logged in. Here, we will tell users about our app and "
+            "include a button which will redirect them to the login screen."
         )

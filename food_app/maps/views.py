d37eabb711189fca6d20706f056efc5d5e5ce9ac
William McConnell <williammcconnell333@gmail.com>
2024-09-16 13:23:44 -0400

    Maps Update 1
    Set up Maps app; can access the Maps page

--- food_app/maps/views.py
+++ food_app/maps/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    if request.user.is_authenticated:
+        return HttpResponse(f"Hello, {request.user.get_username()}, welcome to Maps")
+    else:
+        return HttpResponse(
+            "Welcome to the Maps Page! Here you'll be able to see maps of restaurants"
+        )

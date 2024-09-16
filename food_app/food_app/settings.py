fb5d96050ff100ce3483ab10b566768f31a09c87
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-09-16 15:33:46 -0400

    Update settings.py

--- food_app/food_app/settings.py
+++ food_app/food_app/settings.py
@@ -157,9 +157,9 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 # Email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = os.getenv("GOOGLE_USERNAME")
-EMAIL_HOST_PASSWORD = os.getenv("GOOGLE_PASSWORD")
+EMAIL_HOST_PASSWORD = "password"
+EMAIL_PORT = 587

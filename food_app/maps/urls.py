efb485c55f70dba2a1e2667971237664c077756c
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-09-17 10:03:34 -0400

    Update urls.py

--- food_app/maps/urls.py
+++ food_app/maps/urls.py
@@ -5,6 +5,6 @@ from .views import index, map_view
 app_name = "maps"
 
 urlpatterns = [
-    path("index", index, name="index"),
+    path("index/", index, name="map_index"),
     path("map_view/", map_view, name="map_view"),
 ]

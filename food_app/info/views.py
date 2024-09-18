5b700a5d815a73d2cd7685f95d2fbdecfad8ac5b
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-09-18 19:10:34 -0400

    Added some react support, needs to be fixed

--- food_app/info/views.py
+++ food_app/info/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 
 def info_view(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(
-        "This is the info screen. You are seeing this screen because you are not logged in. Here, we will tell users about our app and include a button which will redirect them to the login screen."
-    )
+    return render(request, "info/index.html")

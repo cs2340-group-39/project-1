2dc0a74c482d415ba032c9c600354773f96923d4
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-09-21 00:38:34 -0400

    Update

---
+++ backend/static/app/home.bundle.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,r}from"./assets/globals-BzYxWdTW.js";function o(){return e.jsx(e.Fragment,{children:e.jsx("h1",{children:"This is the Home Page."})})}const n=document.getElementById("root");t(n).render(e.jsx(r.StrictMode,{children:e.jsx(o,{})}));

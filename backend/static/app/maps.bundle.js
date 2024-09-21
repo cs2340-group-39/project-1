2ccb07ee954de2daa2320013a1ceabac26a3ebee
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-09-21 00:36:08 -0400

    Update gitignore

--- backend/static/app/maps.bundle.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,r}from"./assets/globals-BzYxWdTW.js";function s(){return e.jsx(e.Fragment,{children:e.jsx("h1",{children:"This is the Maps Page."})})}const o=document.getElementById("root");t(o).render(e.jsx(r.StrictMode,{children:e.jsx(s,{})}));

050848cd63bf4cd07fb4bd630c109f6f86e15d16
Arjun Sheshadri <asheshadri2005@gmail.com>
2024-09-29 02:12:05 -0400

    Search api complete

--- backend/maps/urls.py
+++ backend/maps/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import maps_view
+from .api import api
 
 app_name = "maps"
 
 urlpatterns = [
-    path("maps_view/", maps_view, name="maps_view"),
+    path("maps_view/", maps_view, name="maps_view"),  # TODO: Mark for deprecation
+    path("api/", api.urls),
 ]

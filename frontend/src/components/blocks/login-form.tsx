88a2a71c01c96782880f70b47d1a6f2366836e43
nicnit <nicpionit@gmail.com>
2024-10-01 00:17:27 -0400

    google buttons on log in and sign up for old google page
    need to update the google page or authentication method still

--- frontend/src/components/blocks/login-form.tsx
+++ frontend/src/components/blocks/login-form.tsx
@@ -5,6 +5,7 @@ import { cn } from "../../lib/utils";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { LinkPreview } from "../ui/link-preview";
+import {IconBrandGoogle} from "@tabler/icons-react";
 
 interface ErrorMessagesProps {
     messages: string[];
@@ -137,7 +138,29 @@ export function LoginForm() {
                     </button>
                 </div>
 
-                <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+                <div
+                    className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full"/>
+
+                <div className="flex flex-col space-y-8">
+                    {/* TODO: Dynamically generate socialaccount providers */}
+                    <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+                        You may choose to sign in with one of the below social account providers.
+                    </p>
+                    <button
+                        className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                        type="button"
+                        onClick={() => {
+                            window.location.href = "/accounts/google/login/";
+                        }}
+
+                    >
+                        <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300"/>
+                        <span className="text-neutral-700 dark:text-neutral-300 text-sm">Google</span>
+                        <BottomGradient/>
+                    </button>
+                </div>
+
             </form>
         </div>
     );
@@ -146,12 +169,14 @@ export function LoginForm() {
 const BottomGradient = () => {
     return (
         <>
-            <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-            <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+            <span
+                className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent"/>
+            <span
+                className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent"/>
         </>
     );
 };
 
-const LabelInputContainer = ({ children, className }: { children: React.ReactNode; className?: string }) => {
+const LabelInputContainer = ({children, className}: { children: React.ReactNode; className?: string }) => {
     return <div className={cn("flex flex-col space-y-2 w-full", className)}>{children}</div>;
 };
